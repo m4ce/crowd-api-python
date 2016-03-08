0b82acd13b75d4c9512c5f8edef5e6a9acc932db
Matteo Cerutti <matteo.cerutti@hotmail.co.uk>
2016-03-08 09:58:53 +0100

    preparing files for packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+version = '0.0.1'
+
+setup(
+  name = 'crowd-api',
+  packages = ['crowd_api', 'examples'],
+  version = version,
+  description = 'Python library for Atlassian Crowd',
+  author = 'Matteo Cerutti',
+  author_email = 'matteo.cerutti@hotmail.co.uk',
+  url = 'https://github.com/m4ce/crowd-api-python',
+  download_url = 'https://github.com/m4ce/crowd-api-python/tarball/%s' % (version,),
+  keywords = ['crowd'],
+  classifiers = [],
+  install_requires = ["requests"]
+)
