5ebc758d9b287315a6e2220bc60b3285909f1afc
Florian Baumann <flo@noqqe.de>
2019-11-28 14:56:34 +0100

    feature: search_user search_group functions

--- crowd_api/__init__.py
+++ crowd_api/__init__.py
@@ -165,6 +165,25 @@ class CrowdAPI:
         else:
             return {"status": False, "code": req.status_code, "reason": req.content}
 
+    def search_group(self, **kwargs):
+        groups = []
+
+        if 'restriction' not in kwargs:
+            raise ValueError("You need to define a certian restriction")
+
+        req = self.api_get(
+            "/search?entity-type=group&restriction={}&max-results={}".format(kwargs['restriction'], kwargs.get('max_results', 1000)))
+
+        if req.status_code == 200:
+            for group in req.json()['groups']:
+                groups.append(group['name'])
+
+            return {"status": True, "groups": groups}
+        if req.status_code == 404:
+            return {"status": False, "groups": []}
+        else:
+            return {"status": False, "code": req.status_code, "reason": req.content}
+
     def get_all_users(self, **kwargs):
         users = []
 
@@ -179,6 +198,25 @@ class CrowdAPI:
             return {"status": False, "users": []}
         else:
             return {"status": False, "code": req.status_code, "reason": req.content}
+
+    def search_user(self, **kwargs):
+        users = []
+
+        if 'restriction' not in kwargs:
+            raise ValueError("You need to define a certian restriction")
+
+        req = self.api_get(
+            "/search?entity-type=user&restriction={}&max-results={}".format(kwargs['restriction'], kwargs.get('max_results', 1000)))
+        if req.status_code == 200:
+            for user in req.json()['users']:
+                users.append(user['name'])
+
+            return {"status": True, "users": users}
+        if req.status_code == 404:
+            return {"status": False, "users": []}
+        else:
+            return {"status": False, "code": req.status_code, "reason": req.content}
+
 
     def set_user_attribute(self, **kwargs):
         if "username" not in kwargs:
