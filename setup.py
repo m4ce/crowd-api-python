17af0abeec2f9fdacc403e4e16ce9ef5d6af3dea
RIMORIT <rick.moritz@mercedes-benz.com>
2023-11-13 16:05:03 +0100

    pep up by removing spaces around param assignments

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup
 version = '0.0.9'
 
 setup(
-  name = 'crowd-api',
-  packages = ['crowd_api'],
-  version = version,
-  description = 'Python library for Atlassian Crowd',
-  author = 'Matteo Cerutti',
-  author_email = 'matteo.cerutti@hotmail.co.uk',
-  url = 'https://github.com/m4ce/crowd-api-python',
-  download_url = 'https://github.com/m4ce/crowd-api-python/tarball/%s' % (version,),
-  keywords = ['crowd'],
-  classifiers = [],
-  install_requires = ["requests"]
+  name='crowd-api',
+  packages=['crowd_api'],
+  version=version,
+  description='Python library for Atlassian Crowd',
+  author='Matteo Cerutti',
+  author_email='matteo.cerutti@hotmail.co.uk',
+  url='https://github.com/m4ce/crowd-api-python',
+  download_url='https://github.com/m4ce/crowd-api-python/tarball/%s' % (version,),
+  keywords=['crowd'],
+  classifiers=[],
+  install_requires=["requests"]
 )
